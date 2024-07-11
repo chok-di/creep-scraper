ef83131a806e68d7d30273bc474fbb742947a385
Di Wu <dwu233@gmail.com>
2024-07-11 11:59:53 -0400

    add scraping of fpId

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import puppeteer, { Browser, Page } from 'puppeteer';
 
 const CREEP_URL = 'https://abrahamjuliot.github.io/creepjs/';
+const FP_ID_LENGTH = 64;
 
 const scrapeData = async () => {
   const browser: Browser = await puppeteer.launch({
@@ -8,17 +9,39 @@ const scrapeData = async () => {
   });
 
   const page: Page = await browser.newPage();
-
   try {
     await page.goto(CREEP_URL);
-    // wait for 5 seconds
+    // Wait for the element that contains finger print data to be present before proceeding
+    await page.waitForSelector('#fingerprint-data');
+
+    // wait for the animation to finish before the fp data is revealed
     await page.evaluate(() => {
       return new Promise((resolve) => setTimeout(resolve, 5000));
     });
+
+    const result = await page.evaluate(() => {
+      // Scrape fpId
+      const fingerprintInfo = document.querySelector('#fingerprint-data');
+      let fpId: string | null = null;
+      if (fingerprintInfo) {
+        const divs = fingerprintInfo.querySelectorAll('div');
+        for (const div of divs) {
+          if (div.textContent && div.textContent.includes('FP ID:')) {
+            const spans = div.querySelectorAll('span');
+            fpId = Array.from(spans)
+              .map((span) => span.textContent)
+              .join('')
+              .slice(0, FP_ID_LENGTH);
+            break;
+          }
+        }
+      }
+      return { fpId };
+    });
+    console.log(result);
   } catch (err) {
     console.log(err);
   } finally {
-    console.log('successfully access the browser');
     await browser.close();
   }
 };
